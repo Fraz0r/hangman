f234a2e394792722987f33b8d57c27d40fd692e7
Tim Frazier <tim.frazier@gmail.com>
2023-06-28 20:30:03 -0500

    chore: Remove unused scaffold

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,4 @@
 import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
 export default function AboutScreen() {
@@ -8,7 +6,7 @@ export default function AboutScreen() {
     <View style={ styles.container }>
       <Text style={ styles.title }>About</Text>
       <View style={ styles.separator } lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text>This is a demo</Text>
     </View>
   );
 }
